21c463d53a1a5f867976ee0317280e63b4d3086a
Alexey Klieshchunov <isengrym121@gmail.com>
2023-01-11 23:13:33 +0200

    Initial commit

---
+++ src/main/java/ua/klieshchunov/lection911/Lection911Application.java
@@ -0,0 +1,13 @@
+package ua.klieshchunov.lection911;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lection911Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lection911Application.class, args);
+	}
+
+}
---
+++ src/test/java/ua/klieshchunov/lection911/Lection911ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.klieshchunov.lection911;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lection911ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
